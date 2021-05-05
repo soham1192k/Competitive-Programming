ccf7cd3969a7218cab21429a1c72f4201b328262
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-05-05 21:47:08 +0530

    C.cpp added

---
+++ Codeforces Round 719/C.cpp
@@ -0,0 +1,72 @@
+//Soham
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=1e9+7;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+1;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//******************************************************************************************************************************************
+void solve(){
+    int n;cin>>n;
+    if(n==1) {cout<<1<<'\n';return;}
+    if(n==2) {cout<<-1<<'\n';return;}
+    vector<vector<int>>v(n,vector<int>(n));
+    for(int i=0;i<n;i++) v[i][i]=i+1;
+    int curr=n+1;
+    int row=1;int col=0;
+    for(int i=row;i<=n-2;i++){
+        col=0;
+        for(int r=i;r<=n-1;r++){
+            v[r][col]=curr++;
+            col++;
+        }
+    }
+    row=0;col=1;
+    for(int i=col;i<=n-2;i++){
+        row=0;
+        for(int c=i;c<=n-1;c++){
+            v[row][c]=curr++;
+            row++;
+        }
+    }
+    v[0][n-1]=n*n;
+    v[n-1][0]=n*n-1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++) cout<<v[i][j]<<" ";
+        cout<<'\n';
+    }
+}   
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
