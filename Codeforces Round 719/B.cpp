9a2814ba2c305e386dfdad66b41a897a0e0cf2bb
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-05-05 21:46:51 +0530

    B.cpp added

---
+++ Codeforces Round 719/B.cpp
@@ -0,0 +1,68 @@
+//Soham
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=1e9+7;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+1;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//******************************************************************************************************************************************
+int dig(int n){
+    int cnt=0;
+    while(n!=0){
+        n/=10;cnt++;
+    }
+    return cnt;
+}
+int mostsig(int n){
+    int most=0;
+    while(n!=0){
+        most=n%10;
+        n/=10;
+    }
+    return most;
+}
+void solve(){
+    int n;cin>>n;
+    int digcount=dig(n);
+    int ans=(digcount-1)*9+mostsig(n)-1;
+    int curr=0;
+    for(int i=1;i<=digcount;i++){
+        curr=10*curr+mostsig(n);
+    }
+    if(curr<=n) ans++;
+    cout<<ans<<'\n';
+}   
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
