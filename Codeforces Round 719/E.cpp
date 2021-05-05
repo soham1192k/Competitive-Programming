a04b9092f7247f110064a9b2c9ff17e986e44e33
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-05-05 21:47:43 +0530

    E.cpp added

---
+++ Codeforces Round 719/E.cpp
@@ -0,0 +1,70 @@
+//Soham
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=1e9+7;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+1;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//******************************************************************************************************************************************
+void solve(){
+    int n;cin>>n;
+    string s;cin>>s;
+    vector<int>v;
+    for(int i=0;i<n;i++){
+        if(s[i]=='*') v.push_back(i+1);
+    }
+    if(v.size()==0||v.size()==1) {cout<<0<<'\n';return;}
+    debug(v);
+    n=(int)v.size();
+    debug(n);
+    int med=v[(n-1)/2];
+    int idx=(n-1)/2;
+    debug(med);
+    int ans=0;
+    for(int i=0;i<v.size();i++){
+        int gap=abs(idx-i);
+        int should=0;
+        if(idx<=i){
+            should=med+gap;
+        }
+        else{
+            should=med-gap;
+        }
+        ans+=abs(should-v[i]);
+    }
+    cout<<ans<<'\n';
+}   
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
