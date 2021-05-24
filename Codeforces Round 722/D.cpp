bab938cdb921f08d564fa247d17e378ca86db0f3
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-05-25 01:52:05 +0530

    Create D.cpp

---
+++ Codeforces Round 722/D.cpp
@@ -0,0 +1,62 @@
+//Soham
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=998244353;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+2;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//***************************************************************************************************************************************************************
+ 
+void solve(){
+    int n;cin>>n;
+    int dp[n+1];
+    dp[0]=1;
+    dp[1]=1;
+    if(n==1) {cout<<1;return;}
+    int sum=2;
+    int d[n+1];
+    memset(d,0,sizeof(d));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j * i <= n; j++) d[i*j]++;
+    }
+    for(int i=2;i<=n;i++){
+        dp[i]=sum;
+        dp[i]=((dp[i]+d[i]-1)%mod+mod)%mod;
+        sum=(sum+dp[i])%mod;
+    }
+    cout<<dp[n];
+}   
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    // cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
