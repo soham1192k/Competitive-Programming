7ba510cc6cddd28912b6a8893070b5215766d476
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-05-25 01:51:35 +0530

    Create B.cpp

---
+++ Codeforces Round 722/B.cpp
@@ -0,0 +1,61 @@
+//Soham
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=1e9+7;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+2;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//***************************************************************************************************************************************************************
+ 
+void solve(){
+    int n;cin>>n;
+    vector<int>a(n);
+    for(int&x:a) cin>>x;
+    int minn=2e9+2;
+    vector<int>hold;
+    sort(a.begin(),a.end());
+    for(int i=0;i<n-1;i++){
+        minn=min(minn,abs(a[i]-a[i+1]));
+        hold.push_back(minn);
+    }
+    int ans=1;
+    for(int i=0;i<hold.size();i++){
+        if(a[i+1]<=hold[i]) ans++;
+        else break;
+    }
+    cout<<ans<<'\n';
+}   
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
