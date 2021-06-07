c1e289afb12f70fc448bf23e92314077b76ce9c2
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-06-07 12:47:07 +0530

    Create A.cpp

---
+++ Educational Round 110/A.cpp
@@ -0,0 +1,59 @@
+//Soham
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=998244353;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+2;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//*********************************************************************************************************************************************************************************************************
+ 
+void solve(){
+    int a,b,c,d;cin>>a>>b>>c>>d;
+    int maxx=max({a,b,c,d});
+    int maxx2=0;
+    if(a!=maxx)
+    maxx2=max(maxx2,a);
+    if(b!=maxx)
+    maxx2=max(maxx2,b);
+    if(maxx!=c)
+    maxx2=max(maxx2,c);
+    if(maxx!=d)
+    maxx2=max(maxx2,d);
+    if(maxx==a&&maxx2==b||maxx==c&&maxx2==d||maxx==b&&maxx2==a||maxx==d&&maxx2==c) cout<<"NO\n";
+    else cout<<"YES\n";
+}   
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
+ 
