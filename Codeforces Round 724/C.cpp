fe5ca9278e54a4129eadd83900c71bec2d0f0d8b
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-06-07 12:49:23 +0530

    Create C.cpp

---
+++ Codeforces Round 724/C.cpp
@@ -0,0 +1,73 @@
+//Soham
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=998244353;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+2;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//*********************************************************************************************************************************************************************************************************
+ 
+void solve(){
+    int n;cin>>n;
+    string s;cin>>s;
+    map<pair<int,int>,int>mp;
+    int d=0,k=0;
+    int dp[n];
+    dp[0]=1;
+    if(s[0]=='D'){
+        mp[{1,0}]=1;
+        d++;
+    }
+    else{
+        mp[{0,1}]=1;
+        k++;
+    }
+    for(int i=1;i<n;i++){
+        if(s[i]=='D'){
+            d++;
+        }
+        else{
+            k++;
+        }
+        int g=__gcd(d,k);
+        pair<int,int>now={d/g,k/g};
+        dp[i]=1+mp[now];
+        mp[now]++;
+    }
+    for(auto x:dp) cout<<x<<" ";
+    cout<<'\n';
+}   
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
