713608e003dc34b1d4c54c9332946310782628bc
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-06-07 12:47:23 +0530

    Create B.cpp

---
+++ Educational Round 110/B.cpp
@@ -0,0 +1,67 @@
+//Soham
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=998244353;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+2;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//*********************************************************************************************************************************************************************************************************
+ 
+void solve(){
+    int n;cin>>n;
+    vector<int>a(n);
+    for(int&x:a) cin>>x;
+    vector<int>b;
+    for(int i=0;i<n;i++){
+        if(a[i]%2==0) b.push_back(a[i]);
+    }
+    for(int i=0;i<n;i++){
+        if(a[i]%2!=0) b.push_back(a[i]);
+    }
+    int cnt=0;
+    for(int i=0;i<n-1;i++){
+        // int cnt=0;
+        for(int j=i+1;j<n;j++){
+            if(__gcd(b[i],2*b[j])>1){
+                cnt++;
+            }
+        }
+        // cout<<cnt<<" ";
+    }
+    cout<<cnt<<'\n';
+}   
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
+ 
