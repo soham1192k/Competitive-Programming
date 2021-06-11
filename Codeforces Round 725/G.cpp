1c1146e762ea4c70ea41c4034f43916b10cf35eb
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-06-11 21:19:02 +0530

    Create G.cpp

---
+++ Codeforces Round 725/G.cpp
@@ -0,0 +1,69 @@
+//Soham
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=1000000007;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+2;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//*********************************************************************************************************************************************************************************************************
+bool ok(int mid,int x,int y,int a,int b){
+    int right = floorl((x-mid*b)*1.0l/(a-b));
+    int left = ceill((y-mid*a)*1.0l/(b-a));
+    if(max(0ll,left)<=min(right,mid)){
+        return true;
+    }
+    return false;
+}
+void solve(){
+    int x,y,a,b;cin>>x>>y>>a>>b;
+    if(a==b){
+        cout<<min(x,y)/a<<'\n';return;
+    }
+    if(a<b) swap(a,b);
+    int low=0;
+    int high=2e9;
+    int ans=0;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(ok(mid,x,y,a,b)){
+            ans=mid;
+            low=mid+1;
+        }
+        else high=mid-1;
+    }
+    cout<<ans<<'\n';
+}
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
