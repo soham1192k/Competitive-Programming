f378abc21192570bd7ef83232aa60ade1c093b77
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-06-19 19:22:05 +0530

    Create E1.cpp

---
+++ Codeforces Round 726/E1.cpp
@@ -0,0 +1,62 @@
+//Soham
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=1000000007;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+2;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//*********************************************************************************************************************************************************************************************************
+ 
+void solve(){
+    int n,k;cin>>n>>k;    
+    string s;cin>>s;
+    string ans="";
+    for(int i=0;i<n;i++){
+        string curr=s.substr(0,i+1);
+        int now=i+1;
+        while(now<k){
+            now*=2;
+            curr+=curr;
+        }
+        string temp=curr.substr(0,k);
+        if(ans.length()==0) ans=temp;
+        else if(temp<ans){
+            ans=temp;
+        }
+    }
+    cout<<ans<<'\n';
+}
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    // cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
