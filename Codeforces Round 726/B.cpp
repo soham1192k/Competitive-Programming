8d594353260a756032f7c5594e44a90d51725093
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-06-19 19:21:21 +0530

    Create B.cpp

---
+++ Codeforces Round 726/B.cpp
@@ -0,0 +1,67 @@
+//Soham
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=1000000007;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+2;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//*********************************************************************************************************************************************************************************************************
+int distance(int x,int y,int z,int k){
+    return abs(x-z)+abs(y-k);
+}
+void solve(){
+    int n,m,sx,sy;
+    cin>>n>>m>>sx>>sy;
+    vector<int>v1={1,1,n,n};
+    vector<int>v2={1,m,1,m};
+    vector<int>hold(4,0);
+    int ans=-1e9;
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            int now=distance(v1[i],v2[i],v1[j],v2[j])+distance(v1[i],v2[i],sx,sy)+distance(v1[j],v2[j],sx,sy);
+            if(max(ans,now)==now){
+                ans=now;
+                hold[0]=v1[i];
+                hold[1]=v2[i];
+                hold[2]=v1[j];
+                hold[3]=v2[j];
+            }
+        }
+    }
+    for(auto x:hold) cout<<x<<" ";
+    cout<<'\n';
+}
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
