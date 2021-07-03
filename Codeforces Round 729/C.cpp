eabd3b52ff48f6a206eb35a93635ba9699d74c9d
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-07-04 02:21:37 +0530

    Create C.cpp

---
+++ Codeforces Round 729/C.cpp
@@ -0,0 +1,60 @@
+//soham
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=1e9+7;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+1;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//******************************************************************************************************************************************************************
+ 
+void solve(){
+    int n;cin>>n;
+    int rem=n;
+    int curr=(2*(n/2))%mod;
+    rem-=n/2;
+    if(n%2==1) {curr=(curr+2)%mod;rem--;}
+    int lcm=2;
+    for(int i=3;;i++){
+        lcm=(lcm*i)/__gcd(lcm,i);
+        int toadd=(i*(rem-n/lcm))%mod;
+        rem=n/lcm;
+        curr=(curr+toadd)%mod;
+        if(rem==0) break;
+    }
+    // cout<<lcm<<" "<<curr<<" "<<rem<<'\n';
+    cout<<curr<<'\n';
+} 
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
