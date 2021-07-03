ec298fc3cef99d2e57dfe5b39d7714f030715282
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-07-04 02:21:26 +0530

    Create B.cpp

---
+++ Codeforces Round 729/B.cpp
@@ -0,0 +1,71 @@
+//soham
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=1e9+7;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+1;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//******************************************************************************************************************************************************************
+ 
+int power(int a,int b){
+    int ans=1;
+    while(b){
+        if(b&1){
+            b--;
+            ans=ans*a;
+        }
+        else{
+            b=b>>1;
+            a=(a*a);
+        }
+    }
+    return ans;
+}
+void solve(){
+    int n,a,b;cin>>n>>a>>b;
+    if(a==1&&(n-1)%b==0) {cout<<"Yes\n";return;}
+    else if(a==1) {cout<<"No\n";return;}
+    int pos=-1;
+    for(int i=0;;i++){
+        if(power(a,i)<=n){
+            int now=n-power(a,i);
+            if(now%b==0) {cout<<"Yes\n";return;}
+        }
+        else break;
+    }
+    cout<<"No\n";
+} 
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
