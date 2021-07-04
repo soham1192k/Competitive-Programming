2cdf3baa2e1df7bd2855725ad15263a5002615af
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-07-05 01:08:15 +0530

    Create C.cpp

---
+++ Atcoder Beginner Contest 208/C.cpp
@@ -0,0 +1,65 @@
+//soham
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=1e9+7;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+1;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//******************************************************************************************************************************************************************
+ 
+void solve(){
+    int n,k;cin>>n>>k;
+    vector<pair<int,int>>a(n);
+    for(int i=0;i<n;i++){
+        int x;cin>>x;
+        a[i]={x,i+1};
+    }
+    sort(a.begin(),a.end());
+    int ans[n+1];
+    memset(ans,0,sizeof(ans));
+    for(int i=0;i<n;i++){
+        ans[a[i].second]+=k/n;
+    }
+    k%=n;
+    for(int i=0;i<k;i++){
+        ans[a[i].second]++;
+    }
+    // for(int i=0;i<n;i++){
+    //     ans[a[i].second]=a[i].first;
+    // }
+    for(int i=1;i<=n;i++) cout<<ans[i]<<'\n';
+}    
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    // cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
