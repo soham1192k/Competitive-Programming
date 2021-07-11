8d46f6906ca3b39eec0a35a5691aaf5197145628
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-07-11 16:40:00 +0530

    Create D.cpp

---
+++ Codeforces Round 731/D.cpp
@@ -0,0 +1,68 @@
+//soham
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define soham1192k ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<'\n';
+#else
+#define debug(x)
+#endif
+const int mod=1e9+7;
+const double pi = 3.14159265358979323846;
+const int mxN=2e5+1;
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//******************************************************************************************************************************************************************
+ 
+void solve(){
+	int n;cin>>n;
+    vector<int>a(n);
+    for(int&x:a) cin>>x;
+    vector<int>b(n);
+    b[0]=0;
+    int prev=a[0];
+    for(int i=1;i<n;i++){
+        int now=0;
+        for(int bit=0;bit<=29;bit++){
+            if(prev&(1ll<<bit)){
+                //need a set bit
+                if(a[i]&(1ll<<bit)) ;
+                else now+=(1ll<<bit);
+            }
+            else{
+                ;
+            }
+        }
+        b[i]=now;
+        prev=now^a[i];
+    }
+    for(auto x:b) cout<<x<<" ";
+    cout<<'\n';
+}            
+int32_t main(){
+    soham1192k;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r", stdin);
+    freopen("output.txt","w", stdout);
+    freopen("error.txt","w",stderr);
+#endif  
+    int T=1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
